c1fdcc33c766bdfc3a41b618cc6b56c854a712ee
Leonardo Giovanetti <leorou@gmail.com>
2015-04-03 01:07:04 -0300

    Adding JSPM and needed packages

---
+++ app/config.js
@@ -0,0 +1,17 @@
+System.config({
+  "baseURL": "/",
+  "paths": {
+    "*": "*.js",
+    "github:*": "jspm_packages/github/*.js"
+  }
+});
+
+System.config({
+  "map": {
+    "angular/quickstart": "github:angular/quickstart@master",
+    "soundcloud/waveformjs": "github:soundcloud/waveformjs@master",
+    "traceur": "github:jmcriffey/bower-traceur@0.0.87",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.87"
+  }
+});
+
